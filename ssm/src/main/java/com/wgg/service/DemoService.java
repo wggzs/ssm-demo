693ea5dbf4a543f03cf7aa1164bbe809df3cd8f7
wgg <37100163+wggzs@users.noreply.github.com>
2021-11-18 23:01:01 +0800

    Add files via upload

---
+++ ssm/src/main/java/com/wgg/service/DemoService.java
@@ -0,0 +1,9 @@
+package com.wgg.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoService {
+
+}
